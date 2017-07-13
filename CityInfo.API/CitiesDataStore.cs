40d7b96c49d1e478eeae9aef3e67d284b36e5235
Dumitru Casap <dumitru.casap@accesa.eu>
2017-07-13 11:45:15 +0300

    First commit

---
+++ CityInfo.API/CitiesDataStore.cs
@@ -0,0 +1,64 @@
+﻿using CityInfo.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CityInfo.API
+{
+    public class CitiesDataStore
+    {
+        public static CitiesDataStore Current {get;} = new CitiesDataStore();
+
+        public List<CityDto> Cities { get; set; }
+
+        public CitiesDataStore()
+        {
+            Cities = new List<CityDto>()
+            {
+                new CityDto()
+                {
+                    Id = 1,
+                    Name = "Cluj",
+                    Description = "The best",
+                    PointsOfInterest = new List<PointOfInterestDto> ()
+                    {
+                        new PointOfInterestDto()
+                        {
+                            Id = 1,
+                            Name = "Cathedral",
+                            Description = "A historical place"
+                        },
+                        new PointOfInterestDto()
+                        {
+                            Id = 2,
+                            Name = "Piata Unirii",
+                            Description = "Beautiful place"
+                        }
+                    }
+                },
+                new CityDto()
+                {
+                    Id = 2,
+                    Name = "Bucuresti",
+                    Description = "The capital of Romania",
+                     PointsOfInterest = new List<PointOfInterestDto> ()
+                    {
+                        new PointOfInterestDto()
+                        {
+                            Id = 3,
+                            Name = "Central Park",
+                            Description = "Good for rest"
+                        }
+                    }
+                },
+                new CityDto()
+                {
+                    Id = 3,
+                    Name = "Iasi",
+                    Description = "A beautiful city"
+                }
+            };
+        }
+    }
+}
---
+++ CityInfo.API/Controllers/CitiesController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CityInfo.API.Controllers
+{
+    [Route("api/cities")]
+    public class CitiesController : Controller
+    {
+        [HttpGet()]
+        public IActionResult GetCities()
+        {
+            var citiesToReturn = CitiesDataStore.Current.Cities;
+            return Ok(citiesToReturn);
+        }
+        [HttpGet("{id}")]
+        public IActionResult GetCity(int id)
+        {
+            var cityToReturn = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == id);
+
+            if(cityToReturn == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(cityToReturn);
+        }
+
+    }
+}
